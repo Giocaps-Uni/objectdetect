5aa6484ca196fbad7cc77468009f89e0e6699d1e
giovanni <254809@studenti.unimore.it>
2024-02-22 12:25:28 +0100

    Added androidx navigation between fragments

--- app/src/main/java/com/example/objectdetect/MainActivity.java
+++ app/src/main/java/com/example/objectdetect/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.objectdetect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+    private NavHostFragment navHostFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
